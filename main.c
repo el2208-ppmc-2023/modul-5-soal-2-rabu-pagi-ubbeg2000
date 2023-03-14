98e3e90b4a475371d11e81095b99fc0902e662e0
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-03-14 14:05:02 +0700

    Update main.c

--- main.c
+++ main.c
@@ -10,25 +10,38 @@
 #include <stdlib.h>
 
 // Fungsi display memindahkan donat dari tiang awal ke tiang tujuan
-void printOutPerpindahan(..., ..., ..., ...) {
-    printf("Noor memindahkan donat %d dari tiang %c ke tiang %c\n", ..., ..., ...);
-
+void printOutPerpindahan(int n, char awal, char tujuan, int *sum) {
+    printf("Noor memindahkan donat %d dari tiang %c ke tiang %c\n", n, awal, tujuan);
+    (*sum)++;
 }
 
-// Fungsi rekursif untuk menyelesaikan masalah pemindahan menara donat
-void pindahkanDonat(..., ..., ..., ..., ...) {
-    ...
+// Fungsi rekursif untuk menyelesaikan masalah hanoi tower
+void pindahkanDonat(int n, char awal, char tujuan, char bantu, int *sum) {
+    if (n == 1) {
+        printOutPerpindahan(n, awal, tujuan, sum);
+        return;
+    }
+    pindahkanDonat(n-1, awal, bantu, tujuan, sum);
+    printOutPerpindahan(n, awal, tujuan, sum);
+    pindahkanDonat(n-1, bantu, tujuan, awal, sum);
 }
 
 int main() {
-    ...
+    int n;
+    int sum = 0;
     printf("Masukkan jumlah donat (minimal 3): ");
-    scanf("%d", ...);
-    printf("Jumlah donat harus positif!");
-    printf("Jumlah donat minimal 3 yaa!\n");
+    scanf("%d", &n);
+    if (n<0) {
+        printf("Jumlah donat harus positif!");
+        return 0;
+    }
+    if (n<3) {
+        printf("Jumlah donat minimal 3 yaa!\n");
+        return 0;
+    }
 
-    printf("Langkah untuk menyelesaikan masalah menara donat dengan %d donat:\n", ...);
-    pindahkanDonat(..., ..., ..., ..., ...);
-    printf("Jumlah langkah minimal yang diperlukan Noor untuk memindahkan menara donat dari tiang A ke tiang C : %d\n",...);
+    printf("Langkah untuk menyelesaikan masalah menara donat dengan %d donat:\n", n);
+    pindahkanDonat(n, 'A', 'C', 'B', &sum);
+    printf("Jumlah langkah minimal yang diperlukan Noor untuk memindahkan menara donat dari tiang A ke tiang C : %d\n",sum);
     return 0;
 }
